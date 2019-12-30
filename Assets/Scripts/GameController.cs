834195544f4a71cb8d9f7f2a1ca14f250d340d07
hamlet <imhamlet.hams@gmail.com>
2019-12-30 10:57:21 +0545

    almost final version

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+
+	public int hazardCount;
+
+	public float spawnWait;
+	public float startWait;
+
+	public Vector3 spawnValue;
+
+	void Start(){
+		StartCoroutine (SpawnWaves ());
+	}
+
+	IEnumerator SpawnWaves(){
+
+		yield return new WaitForSeconds (startWait);
+
+		for(int i=0; i<hazardCount; i++){
+
+			Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValue.x,spawnValue.x),spawnValue.y,spawnValue.z);
+			Quaternion spawnRotation = Quaternion.identity;
+
+			Instantiate (hazard, spawnPosition, spawnRotation);
+
+			yield return new WaitForSeconds (spawnWait);
+		}
+	}
+
+}
